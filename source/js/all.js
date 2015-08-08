2e71d4167fb1bd22c2952365560efffd8cdfaf92
dearty <adrian@dearty.com>
2015-08-08 15:31:20 +0200

    some changes

--- source/js/all.js
+++ source/js/all.js
@@ -1,6 +1,12 @@
 
 //= require classie
 //= require menu_j
+//= require smoothscroll
+
+
+
+
+
 
 function addListener(element, type, callback) {
  if (element.addEventListener) element.addEventListener(type, callback);
@@ -11,13 +17,13 @@ var cta_link_1 = document.getElementById('cta-1');
 var cta_link_2 = document.getElementById('cta-2');
 if(cta_link_1) {   //if the element exists add the click event
 	addListener(cta_link_1, 'click', function() {
-	  ga('send', 'event', 'button', 'click', 'cta-buttons');
+	  ga('send', 'event', 'button', 'click', 'cta-1');
 	});	
 };
 
 if(cta_link_2) {
 	addListener(cta_link_2, 'click', function() {
-	  ga('send', 'event', 'button', 'click', 'cta-buttons');
+	  ga('send', 'event', 'button', 'click', 'cta-2');
 	});
 };
 

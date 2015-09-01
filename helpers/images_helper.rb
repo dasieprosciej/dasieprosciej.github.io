aef373ae05c0f137846ba697b935fce848b4e0a1
dearty <mail@dasieprosciej.pl>
2015-09-01 14:31:36 +0200

    commit

--- helpers/images_helper.rb
+++ helpers/images_helper.rb
@@ -29,6 +29,23 @@ module ImagesHelper
     tag(:img, options)
   end
 
+  def dataimg(url, options={})
+    options.reverse_merge!(:data => {:src => imgpath(url)})
+    tag(:img, options)
+  end
+
+  def bgimg(url, options={}, &block)
+
+    options.reverse_merge!('data-background-image' => imgpath(url), :class => 'bg-img')
+    content = block_given? ? capture(&block) : ''
+    content_tag :section, content, options
+    # content_tag = tag(:section, options, capture(&block))
+    # content
+    # capture(&block)
+
+    # "data-background-image='" + imgpath(url) + "'"
+  end
+
   # Acts as a thin wrapper for image_tag and generates an srcset attribute for regular image tags
   # for usage with responsive images polyfills like picturefill.js, supports asset pipeline path helpers.
   #
@@ -40,5 +57,64 @@ module ImagesHelper
     srcset = srcset.map { |src, size| "#{imgpath(src)} #{size}" }.join(', ')
     image_tag(imgpath(source), options.merge(srcset: srcset))
   end
+
+
+  def figure(content_url, caption=nil, options={})
+      content = img(content_url, options)
+
+    if caption
+      content += content_tag(:figcaption, caption)
+    end  
+    content_tag :figure, content
+  end
+
+  def figure(type, content_url, caption=nil, options={})
+    if !(type == :s || type == :m || type == :l || type == :xl || type== :responsive) && options[:type].blank?
+      raise ArgumentError.new("You should pass :type option key explicitly, because you have passed #{type} type other than :s, :m, :l, :xl or :responsive.")
+    end
+
+    case type
+    when :s
+      suffix = '-s'
+    when :m 
+      suffix = '-m'
+    when :l 
+      suffix = '-l'
+    when :xl
+      suffix = '-xl'
+    when :responsive
+       suffix = ''
+    end
+    
+    basename = File.basename(content_url, '.*')
+    ext = File.extname(content_url)
+
+    real_path = content_url.sub(basename + ext,basename + suffix + ext )
+    # real_path = File.join(images_dir, real_path) unless real_path.start_with?('/')
+    # full_path = File.join(source_dir, real_path)
+
+    if type == :responsive
+      content = dataimg(content_url, options) 
+      # dataimg z gory
+    else        
+      content = img(real_path, options)
+    end  
+
+    if caption
+      content += content_tag(:figcaption, caption)
+    end  
+    content_tag :figure, content
+  end
+
+  # def figure_tag(content_type, content_urls=[], caption, options={})
+  #   case content_type
+  #   when :image
+  #     content = image_tag(content_urls.first, srcset: content_urls.join(','), alt: options[:alt])
+  #   when :video
+  #     content = video_tag(content_urls.first, alt: options[:alt], controls: true)
+  #   end 
+  #   content += content_tag(:figcaption, caption)
+  #   content_tag :figure, content
+  # end
 
 end
